0ff0cf21b711a8d52a0bd37606678d0d9c8b99a3
KirAlex008 <kirshaav@yandex.ru>
2023-05-09 20:06:54 -0400

    6. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -2,52 +2,26 @@ package ru.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
 
+    char[] process = new char[] {'-', '\\', '|', '/'};
+
     @Override
     public void run() {
-        int index = 0;
         while (!Thread.currentThread().isInterrupted()) {
-            try {
-                while (index < 6) {
-                    marking();
-                    index++;
+            for (var sing :  process) {
+                System.out.print("\r load: " + sing);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
                 }
-                System.out.print("\rLoaded.");
-                Thread.currentThread().interrupt();
-
-                    /*System.out.print("\r load: " + "\\");
-                    //System.out.print("\rLoading : " + index  + "%");
-                    Thread.sleep(500);
-                    System.out.print("\r load: " + "|");
-                    Thread.sleep(500);
-                    System.out.print("\r load: " + "/");
-                    Thread.sleep(500);*/
-
-
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
-        progress.run();
-        try {
-            Thread.sleep(10); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+        progress.start();
+        Thread.sleep(5000);
         progress.interrupt();
-    }
-
-    public void marking() throws InterruptedException {
-        String[] sings = new String[3];
-        sings[0] = "\\";
-        sings[1] = "|";
-        sings[2] = "/";
-        for (int i = 0; i <= 2; i++) {
-            System.out.println("\r load: " + sings[i]);
-            Thread.sleep(500);
-        }
     }
 }
